af40fcedeb0f73766f62d687d4f9d6ef4bd0359f
VVeaz <karolina_wlaz@wp.pl>
2019-04-04 14:28:20 +0200

    add method learn

--- src/Neuron.java
+++ src/Neuron.java
@@ -22,10 +22,21 @@ class Neuron {
 
     }
     int stepFunction(){
-          System.out.println(weight);
+        //  System.out.println(weight);
         if(weightedSum>=0.7)
             return 1;
         return 0;
+    }
+    void learn(int[]x0, int[]x1, int[] answer, double learningCoefficient ){
+        Random generatorIndex = new Random();
+        for(int i=0; i<50000; i++){
+            int index = generatorIndex.nextInt(x0.length);
+            calculate(x0[index],x1[index]);
+            double newWeightX0 = weight.get(0)+x0[index]*(answer[index]-stepFunction())*learningCoefficient;
+            double newWeightX1 = weight.get(1)+x1[index]*(answer[index]-stepFunction())*learningCoefficient;
+            weight.set(0, newWeightX0);
+            weight.set(1,newWeightX1);
+        }
     }
 
 }
